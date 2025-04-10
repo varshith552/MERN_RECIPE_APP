1dfebac0e96a72226744fe53cd98725def90c293
varshith552 <varshithsaianishetti03@gmail.com>
2025-04-10 18:30:12 +0530

    Add files via upload
    added

---
+++ client/src/pages/home.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+
+export const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("https://recipe-app-kh93.onrender.com/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `https://recipe-app-kh93.onrender.com/recipes/savedRecipes/ids/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchRecipes();
+    fetchSavedRecipes();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put("https://recipe-app-kh93.onrender.com/recipes", {
+        recipeID,
+        userID,
+      });
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const isRecipeSaved = (id) => savedRecipes.includes(id);
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <button
+                onClick={() => saveRecipe(recipe._id)}
+                disabled={isRecipeSaved(recipe._id)}
+              >
+                {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
+              </button>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://demo123:demo123@cluster0.v6pmhez.mongodb.net/rec_db?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
